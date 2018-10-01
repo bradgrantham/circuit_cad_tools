377ea9de0b8db50a53bfc37f1137caea5999c9a0
Brad Grantham <grantham@plunk.org>
2018-10-01 08:19:22 -0700

    extend to Brad's full BOM fields

--- coalese_kicad_bom.py
+++ coalese_kicad_bom.py
@@ -1,10 +1,30 @@
 import csv
 import sys
 
-bom = {}
-for row in csv.reader(sys.stdin):
-# Reference, Value, Footprint, Datasheet, Part
-    bom.setdefault(" ".join([row[1], row[2]]), []).append(row)
+# Brad's fields: Reference, Value, Footprint, Datasheet, Manufacturer, Manufacturer PN, Distributor, Distributor PN, Distributor URL, Description, Package, Type
 
-for (part, refs) in bom.iteritems():
-    print part
+bom = {}
+
+csvreader = csv.reader(sys.stdin)
+
+headers = csvreader.next()[1:]
+headers.insert(0, 'Item')
+headers.insert(1, 'Ref Des')
+headers.insert(2, 'Qty')
+
+for row in csvreader:
+    reference = row[0]
+    otherfields = row[1:-1]
+    key = " ".join(otherfields)
+    bomentry = bom.setdefault(key, [[], otherfields])
+    bomentry[0].append(reference)
+
+print headers
+print ", ".join(headers)
+item = 0
+for (key, (refs, fields)) in bom.iteritems():
+    print '%d, ' % item,
+    print '"%s", ' % (", ".join(refs)), 
+    print '%d, ' % len(refs),
+    print ", ".join(fields)
+    item = item + 1
