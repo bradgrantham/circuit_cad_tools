cc0a752902f0f630454af0baa201960e708e49c0
Brad Grantham <grantham@plunk.org>
2018-10-02 17:21:23 -0700

    validate input fields, improve pythonism
    Use DictReader so input fields can be in any order
    
    Use DictWriter so output can be from a dictionary
    
    Catch case where BOM Fields are interpreted with leading " "
    
    Validate input fields against expected fields

--- coalese_kicad_bom.py
+++ coalese_kicad_bom.py
@@ -1,30 +1,45 @@
 import csv
 import sys
 
-# Brad's fields: Reference, Value, Footprint, Datasheet, Manufacturer, Manufacturer PN, Distributor, Distributor PN, Distributor URL, Description, Package, Type
+# Input fields (for validation)
+input_field_names = ["Reference", "Value", "Footprint", "Do Not Place", "Datasheet", "Manufacturer", "Manufacturer PN", "Distributor", "Distributor PN", "Distributor URL", "Description", "Package", "Type"]
+
+# Output fields
+output_field_names = ["Item", "Ref Des", "Qty", "Value", "Footprint", "Do Not Place", "Datasheet", "Manufacturer", "Manufacturer PN", "Distributor", "Distributor PN", "Distributor URL", "Description", "Package", "Type"]
 
 bom = {}
 
-csvreader = csv.reader(sys.stdin)
+csvreader = csv.DictReader(sys.stdin)
 
-headers = csvreader.next()[1:]
-headers.insert(0, 'Item')
-headers.insert(1, 'Ref Des')
-headers.insert(2, 'Qty')
+for part in csvreader:
+    for (field_name, field_value) in part.iteritems():
+        if field_name[0] == ' ':
+            del part[field_name]
+            field_name = field_name[1:]
+            part[field_name] = field_value
+        if field_name not in input_field_names:
+            print "field '%s' in input file is not in input field_names" % field_name
+            sys.exit(1)
+    reference = part['Reference']
+    unique = part
+    del unique['Reference']
+    for field in unique.iterkeys():
+        if not unique[field]:
+            unique[field] = ""
+    key_parts = [unique.get(field, "") for field in sorted(unique.iterkeys())]
+    key = " ".join(key_parts)
+    entry = bom.setdefault(key, {"refs" : [], "partfields" : unique})
+    entry["refs"].append(reference)
 
-for row in csvreader:
-    reference = row[0]
-    otherfields = row[1:-1]
-    key = " ".join(otherfields)
-    bomentry = bom.setdefault(key, [[], otherfields])
-    bomentry[0].append(reference)
+writer = csv.DictWriter(sys.stdout, output_field_names)
 
-print headers
-print ", ".join(headers)
+writer.writeheader()
 item = 0
-for (key, (refs, fields)) in bom.iteritems():
-    print '%d, ' % item,
-    print '"%s", ' % (", ".join(refs)), 
-    print '%d, ' % len(refs),
-    print ", ".join(fields)
+for (key, entry) in bom.iteritems():
+    bom_entry = entry["partfields"]
+    refs = entry["refs"]
+    bom_entry["Item"] = item
+    bom_entry["Ref Des"] = ",".join(refs)
+    bom_entry["Qty"] = len(refs)
+    writer.writerow(bom_entry)
     item = item + 1
